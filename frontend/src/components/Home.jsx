846995b1a2020995368c8c2ee9572e52386bd747
devvinit <vinit91.dev@gmail.com>
2021-11-30 21:11:29 +0530

    initial commit with hello world

---
+++ frontend/src/components/Home.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+
+export const Home = () => {
+    return(<>
+            <div className="container">
+               <h1 className="hello">Hello World</h1>
+               <button className="newEmp">New Employee</button>
+
+               <select className="deptSelct" name="departments" id="depart">
+                  <option value="Accounts and Finance">Select Department</option>
+                  <option value="Accounts and Finance">Accounts and Finance</option>
+                  <option value="HR">HR</option>
+                  <option value="Research and development">Research and development</option>
+                  <option value="Learning and development">Learning and development</option>
+                  <option value="IT services">IT services</option>
+                  <option value="Product development">Product development</option>
+                  <option value="Admin department">Admin department</option>
+               </select>
+             </div> 
+           </>)
+}
+
